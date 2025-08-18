a0236c675ff85fa07bed7768d2b8e9abbfb55635
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-08-17 22:18:35 -0300

    fix: correct format

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,19 +1,19 @@
-import type { Link } from "@/types/links"
+import type { Link } from '@/types/links'
 
 export const LINKS: Link[] = [
   {
-    title: "GitHub",
-    label: "github",
-    url: "https://github.com/NahuelLescano",
+    title: 'GitHub',
+    label: 'github',
+    url: 'https://github.com/NahuelLescano',
   },
   {
-    title: "LinkedIn",
-    label: "linkedin",
-    url: "https://www.linkedin.com/in/nahuel-lescano-906a2618b/",
+    title: 'LinkedIn',
+    label: 'linkedin',
+    url: 'https://www.linkedin.com/in/nahuel-lescano-906a2618b/',
   },
   {
-    title: "X",
-    label: "x",
-    url: "https://x.com/nahuel0495",
+    title: 'X',
+    label: 'x',
+    url: 'https://x.com/nahuel0495',
   },
 ] as const
--- src/consts/navItems.ts
+++ src/consts/navItems.ts
@@ -1,12 +1,12 @@
-import type { NavItem } from "@/types/navItems"
+import type { NavItem } from '@/types/navItems'
 
 export const NAV_ITEMS: NavItem[] = [
-  { title: "Inicio", label: "inicio", url: "#top" },
-  { title: "Proyectos", label: "proyectos", url: "/#projects" },
-  { title: "Sobre mi", label: "sobre-mi", url: "/#about-me" },
+  { title: 'Inicio', label: 'inicio', url: '#top' },
+  { title: 'Proyectos', label: 'proyectos', url: '/#projects' },
+  { title: 'Sobre mi', label: 'sobre-mi', url: '/#about-me' },
   {
-    title: "Contacto",
-    label: "contacto",
-    url: "mailto:nahuellescano1012@gmail.com",
+    title: 'Contacto',
+    label: 'contacto',
+    url: 'mailto:nahuellescano1012@gmail.com',
   },
 ] as const
