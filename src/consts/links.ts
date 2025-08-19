4130ceb0f64f0fcf8ce499134e576902e05e0b5b
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-08-18 23:39:36 -0300

    feat: add single url for all links

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,5 +1,29 @@
 import type { Link } from '@/types/links'
 
+export const GITHUB_URL: Link = {
+  title: 'GitHub',
+  label: 'github',
+  url: 'https://github.com/NahuelLescano',
+} as const
+
+export const LINKEDIN_URL: Link = {
+  title: 'LinkedIn',
+  label: 'linkedin',
+  url: 'https://www.linkedin.com/in/nahuel-lescano-906a2618b/',
+} as const
+
+export const X_URL: Link = {
+  title: 'X',
+  label: 'x',
+  url: 'https://x.com/nahuel0495',
+} as const
+
+export const EMAIL: Link = {
+  title: 'Contacto',
+  label: 'contacto',
+  url: 'mailto:nahuellescano1012@gmail.com',
+} as const
+
 export const LINKS: Link[] = [
   {
     title: 'GitHub',
