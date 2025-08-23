82f7ec6accbdacda557f0f46128f7a0d5d917443
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-08-23 00:35:57 -0300

    feat: add experience from kromee

---
+++ src/consts/experiences.ts
@@ -0,0 +1,12 @@
+import type { Experience } from '@/types/experience'
+
+export const EXPERIENCES: Experience[] = [
+  {
+    date: 'Actualmente...',
+    title: 'Desarrollador Backend',
+    company: 'Kromee',
+    description:
+      'Desarrollo y mantengo aplicaciones utilizando fastapi, uvicorn y pydantic y bases de datos SQL. Implemento APIs RESTful y colaboro con equipos multifuncionales para entregar soluciones eficientes y escalables.',
+    link: 'https://kromee.com/',
+  },
+] as const
